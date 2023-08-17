08423574b58b21db2c1431463f37d11438250743
Daniele <>
2023-08-18 01:54:02 +0200

    sx

--- Assest/script.js
+++ Assest/script.js
@@ -2,13 +2,14 @@
 
 //--->Menù
 
+
 const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu');
 
 menuIcon.addEventListener('click', () => {
     menu.classList.toggle('active');
+    menuIcon.classList.toggle('open');
 });
-
 
 
 
